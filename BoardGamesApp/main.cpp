6413eb1a31cc7f19b61e3a51d46881667b9928be
jkierucz <01169183@pw.edu.pl>
2022-05-31 11:46:13 +0200

    Added base for communication with Game

--- BoardGamesApp/main.cpp
+++ BoardGamesApp/main.cpp
@@ -12,12 +12,12 @@ int main()
         system("pause");
     }
 
-    sf::RenderWindow window(sf::VideoMode(800, 800), "TEST");
-    Board square(
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Sudoku");
+    Board board(
         sf::Vector2f(100.f, 100.f), 
-        sf::Color::Yellow, 
-        sf::Color::White,
-        sf::Color::Blue,
+        sf::Color::Black, 
+        sf::Color(155, 155, 155, 100),
+        sf::Color::Black,
         font, 
         500,
         9
@@ -28,13 +28,32 @@ int main()
         sf::Event e;
         while (window.pollEvent(e))
         {
-            if (e.type == sf::Event::Closed)
+            switch (unsigned int key = -1;  e.type)
             {
-                window.close();
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                case sf::Event::KeyPressed:
+                    key = e.key.code;
+                    //send to Game? Game sends back if valid?
+                    //board.update();
+                    break;
+
+                case sf::Event::MouseButtonPressed:
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+                    {
+                        //board.getTile(sf::Mouse::getPosition(window)).highlight;
+                    }
+                    //board.update(); ?
+                    break;
+
+                default:
+                    break;
             }
         }
-        window.clear();
-        square.draw(window);
+        window.clear(sf::Color::White);
+        board.draw(window);
         window.display();
     }
     return 0;
--- BoardGamesLib/tile.cpp
+++ BoardGamesLib/tile.cpp
@@ -17,7 +17,7 @@ Tile::Tile(
     setPosition(position);
     setFillColor(backgroundColor);
     setSize(sf::Vector2f(size, size));
-    setOutlineThickness(1.f * size * -0.01);
+    setOutlineThickness(5.f * size * -0.01);
     setOutlineColor(frameColor);
     
     displayValue.setFont(font);
