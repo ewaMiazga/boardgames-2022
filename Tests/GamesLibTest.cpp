b3a3cb61864f1647357ed7e930727692d2cbd14c
Ewa Miazga <01169208@pw.edu.pl>
2022-06-02 18:03:49 +0200

    Add: tests from repository GamesTests to project 208.1-gry-planszowe

---
+++ Tests/GamesLibTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\208.1-gry-planszowe\Games.h"
+
+// -------------------------------------------------- Ewa Miazga ----------------------------------------------------------
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace GamesLibTest
+{
+	TEST_CLASS(GamesLibTest)
+	{
+	public:
+
+	};
+}
